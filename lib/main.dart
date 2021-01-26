5e94f07ceab397582716db2389d63bcab8784921
MohamedFaraag <max.farag2017@gmail.com>
2021-01-25 21:37:37 -0800

    :

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Paymssent',
+      title: 'Payment',
       theme: ThemeData(
         primarySwatch: Colors.deepOrange,
       ),
